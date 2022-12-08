d65f8676792c89d581aadbaa1901bcf28bea743f
hrshuv0 <h.r.shuvo9@gmail.com>
2022-12-08 22:28:36 +0600

    Add product entity

---
+++ src/API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    // GET
+    [HttpGet]
+    public string GetProducts()
+    {
+        return "This will be a list of products";
+    }
+
+    [HttpGet("{id:int}")]
+    public string GetProduct(int id)
+    {
+        return "Single product";
+    }
+}
---
+++ src/API/Data/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().ToTable("Product");
+        
+        base.OnModelCreating(modelBuilder);
+    }
+
+
+    public DbSet<Product> Products { get; set; }
+}
