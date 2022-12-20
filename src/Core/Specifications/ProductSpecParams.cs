9aca460f88feaf7b5348b455a12686162db25867
hrshuv0 <h.r.shuvo9@gmail.com>
2022-12-20 18:08:45 +0600

    Add search

--- src/Core/Specifications/ProductSpecParams.cs
+++ src/Core/Specifications/ProductSpecParams.cs
@@ -15,6 +15,14 @@ public class ProductSpecParams
     
     public int? BrandId { get; set; }
     public int? TypeId { get; set; }
-    public string Sort { get; set; }
-    
+    public string? Sort { get; set; }
+
+    private string? _search;
+
+    public string Search
+    {
+        get => _search!; 
+        set => _search = value.ToLower();
+    }
+
 }
--- src/Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
+++ src/Core/Specifications/ProductsWithTypesAndBrandSpecification.cs
@@ -5,7 +5,9 @@ namespace Core.Specifications;
 public class ProductsWithTypesAndBrandSpecification : BaseSpecification<Product>
 {
     public ProductsWithTypesAndBrandSpecification(ProductSpecParams productParams) : base(x => 
-        (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
+        (string.IsNullOrEmpty(productParams.Search) || x.Name!.ToLower().Contains(productParams.Search)) &&
+        (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
+        (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId))
     {
         AddInclude(x => x.ProductBrand!);
         AddInclude(x => x.ProductType!);
