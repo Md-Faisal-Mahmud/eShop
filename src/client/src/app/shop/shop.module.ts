64562ad3138b3f67c17852dd64815508037a3f29
hrshuv0 <h.r.shuvo9@gmail.com>
2022-12-25 19:44:16 +0600

    SPA - Add components

---
+++ src/client/src/app/shop/shop.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ProductDetailsComponent} from "./product-details/product-details.component";
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ShopComponent } from './shop.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductDetailsComponent,
+    ProductItemComponent,
+    ShopComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ShopModule { }
